7c0df4e7cc75e851d95550e20a0c5593ee4c9cdd
karenyoon <yoonyoonsung4@gmail.com>
2023-01-04 17:02:36 +0900

    JPA/JDBC 생성 및 Test Code

--- src/test/java/zerobase/weather/WeatherApplicationTests.java
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -1,13 +1,35 @@
 package zerobase.weather;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeatherApplicationTests {
-
 	@Test
-	void contextLoads() {
+	void equalTest() {
+	    // given
+	    // when
+	    // then
+		assertEquals(1, 1);
+	}
+	@Test
+	void nullTest() {
+	    // given
+	    // when
+	    // then
+		assertNull(null);
 	}
 
+	@Test
+	void trueTest() {
+	    // given
+	    // when
+	    // then
+		assertTrue(1 == 1);
+	}
 }
